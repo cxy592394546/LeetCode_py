9e09144792ea568cd7a7e9eab83c44ba66d95437
xinyu <cxy592394546@gmail.com>
2021-02-21 12:29:56 +0800

    add

---
+++ Code/Daily/2102/210221/1438.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def func(nums: List[int], limit: int) -> int:
+    l, r = 0, 0
+    max_dict, min_dict = {}, {}
+    for i in range(len(nums)):
+
+
+
+
+if __name__ == '__main__':
+    print(func([8, 2, 4, 7], 4))
