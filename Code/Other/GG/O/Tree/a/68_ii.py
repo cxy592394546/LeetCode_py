a0f780f6801944892af19f8909fc26149b5b8a9e
Xinyu <cxy592394546@gmail.com>
2021-05-09 18:05:04 +0800

    add

---
+++ Code/Other/GG/O/Tree/a/68_ii.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left:  # 如果左侧没有目标节点
+            return right
+        if not right:  # 如果右侧没有目标节点
+            return left
+        return root  # 如果两侧都有目标节点
